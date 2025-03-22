ce4fcddcf82a7deccf16ed65f402c044514023c4
disk02 <130608498+disk02@users.noreply.github.com>
2025-03-22 01:32:42 +0000

    Add files via upload

--- genstereo/__init__.py
+++ genstereo/__init__.py
@@ -1,2 +1,18 @@
-from .GenStereo import GenStereo
-from .GenStereo import AdaptiveFusionLayer
+from .GenStereo import GenStereo, AdaptiveFusionLayer  # Add missing import
+from .ops import convert_left_to_right_torch
+from .models import (
+    PoseGuider,
+    UNet2DConditionModel,
+    UNet3DConditionModel,
+    ReferenceAttentionControl
+)
+
+__all__ = [
+    'GenStereo',
+    'AdaptiveFusionLayer',  # Add to exports
+    'convert_left_to_right_torch',
+    'PoseGuider',
+    'UNet2DConditionModel', 
+    'UNet3DConditionModel',
+    'ReferenceAttentionControl'
+]
